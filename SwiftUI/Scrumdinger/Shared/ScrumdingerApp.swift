a661e7e385013c66d0df9accf2aeefefcd8ad5b3
Jhoel Perez <jhoel.jjpr@gmail.com>
2022-06-15 17:10:22 +0300

    Scrumdinger app init
    View for Scrum meeting tracking

---
+++ SwiftUI/Scrumdinger/Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Jhoel Perez Ruiz on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
