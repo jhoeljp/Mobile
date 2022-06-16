6eca4f48dd38d3a1f6dad9f2b8b2045522c5b588
Jhoel Perez <jhoel.jjpr@gmail.com>
2022-06-16 14:49:24 +0300

    Card View
    list of scrum user attendees and meeting length in minutes

---
+++ SwiftUI/Scrumdinger/Shared/Models/Theme.swift
@@ -0,0 +1,39 @@
+//
+//  Theme.swift
+//  Scrumdinger (iOS)
+//
+//  Created by Jhoel Perez Ruiz on 6/16/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavander
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self{
+        case .bubblegum, .buttercup, .lavander, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
